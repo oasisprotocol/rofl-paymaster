53e6f98f736cca4d266890e641ff16434a2d6ff2
rube-de <rube@ruf2.de>
2026-01-19 12:58:06 +0100

    fixd: update pragmas

--- contracts/contracts/sapphire/interfaces/ICrossChainPaymaster.sol
+++ contracts/contracts/sapphire/interfaces/ICrossChainPaymaster.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.24;
 
 import { ReceiptProof } from "../../hashi/prover/HashiProverStructs.sol";
 import { SapphireTypes } from "../libraries/SapphireTypes.sol";
